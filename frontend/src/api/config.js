2a12bbeb97c557254bc00315ac20667c7ac7787d
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-13 18:50:36 +0100

    .

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -2,6 +2,6 @@ export const APISettings = {
     headers: new Headers({
         'Accept': 'application/json'
     }),
-    baseURL: 'http://192.168.1.44:8000'
-    // baseURL: 'https://db-api.gnerd.dk',
+    // baseURL: 'http://192.168.1.44:8000'
+    baseURL: 'https://db-api.gnerd.dk',
 }
